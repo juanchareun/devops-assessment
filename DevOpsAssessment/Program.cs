62cea18c5e7d7de68b009975e3a955bba3b8d47e
juanchareun <juanignaciochareun@gmail.com>
2026-09-23 09:22:27 -0400

    health and version API endpoints

---
+++ DevOpsAssessment/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/health", () =>
+{
+    return Results.Ok(new
+    {
+        status = "healthy"
+    });
+});
+
+app.MapGet("/version", () =>
+{
+    var version = Environment.GetEnvironmentVariable("APP_VERSION") ?? "dev";
+
+    return Results.Ok(new
+    {
+        version
+    });
+});
+
+app.Run();
